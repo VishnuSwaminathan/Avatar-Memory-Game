b0256439fb696f75a43681650f77281fc5863389
VishnuSwaminathan <vishcodes@gmail.com>
2018-11-17 14:34:57 -0500

    Updates

---
+++ precache-manifest.1df7f5ee4fa7d2c636a37a7bbb74ee58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "822c79aae656a64f3fb7",
+    "url": "/Avatar-Memory-Game/static/js/runtime~main.822c79aa.js"
+  },
+  {
+    "revision": "4e2bc60eb737d32daf3a",
+    "url": "/Avatar-Memory-Game/static/js/main.4e2bc60e.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/Avatar-Memory-Game/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "4e2bc60eb737d32daf3a",
+    "url": "/Avatar-Memory-Game/static/css/main.7f70c4e2.chunk.css"
+  },
+  {
+    "revision": "bc0b899d68fa4ae3a212620c27e86b53",
+    "url": "/Avatar-Memory-Game/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Avatar-Memory-Game/precache-manifest.1df7f5ee4fa7d2c636a37a7bbb74ee58.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Avatar-Memory-Game/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
